4d3b7b7d1a796779fab798eacb91653fcbdfeb55
hoangthang668 <70740641+hoangthang668@users.noreply.github.com>
2020-10-09 23:55:07 +0700

    nop bai

--- FinalExam_Java/hoangducthang_FinalExam/src/frontEnd/Program.java
+++ FinalExam_Java/hoangducthang_FinalExam/src/frontEnd/Program.java
@@ -15,8 +15,7 @@ public class Program {
 		do {
 
 			System.out.println("      1. Đăng nhập                   ");
-			System.out.println("      2. Đăng ký                     ");
-			System.out.println("      0. Thoát chương trình          ");
+			System.out.println("      2. Thoát chương trình          ");
 			System.out.println("                                       ");
 			System.out.println();
 			System.out.println("      Xin mời chọn chức năng           ");
@@ -25,10 +24,8 @@ public class Program {
 			case 1:
 				dangNhap();
 				break;
+
 			case 2:
-				dangKy();
-				break;
-			case 0:
 				System.out.println("Đã thoát chương trình!");
 				break;
 			default:
@@ -49,45 +46,5 @@ public class Program {
 			System.out.println("Email hoặc password không đúng. Kiểm tra lại!");
 	}
 
-	private static void dangKy() throws SQLException {
-		boolean check = false;
-		System.out.println("Nhap loai user muon tao (1-Manager ; 0-Employee): ");
-		while (!check) {
-			int loaiUser = ScannerUtils.inputInt("Nhap vao 1 so");
-			if (loaiUser == 1) {
-				dangKyManager();
-				check = true;
-			} else if (loaiUser == 0) {
-				dangKyEmployee();
-				check = true;
-			} else
-				System.out.println("Khong co lua chon nay. Nhap lai!");
-		}
-	}
-
-	private static void dangKyManager() throws SQLException {
-		System.out.println("Nhap FullName:");
-		String fullName = ScannerUtils.inputString("Nhap lai!");
-		System.out.println("Nhap Email:");
-		String email = ScannerUtils.inputString("Nhap lai!");
-		System.out.println("Nhap pass word:");
-		String pass = ScannerUtils.inputString("Nhap lai!");
-		System.out.println("Nhap ExpInYear:");
-		int expInYear = ScannerUtils.inputInt("Nhap lai!");
-		userController.registerManager(fullName, email, pass, expInYear);
-	}
-
-	private static void dangKyEmployee() throws SQLException {
-		System.out.println("Nhap FullName:");
-		String fullName = ScannerUtils.inputString("Nhap lai!");
-		System.out.println("Nhap Email:");
-		String email = ScannerUtils.inputString("Nhap lai!");
-		System.out.println("Nhap Password:");
-		String pass = ScannerUtils.inputString("Nhap lai!");
-		System.out.println("Nhap projectId:");
-		int projectId = ScannerUtils.inputInt("Nhap lai!");
-		System.out.println("Nhap ProSkill:");
-		String proSkill = ScannerUtils.inputString("Nhap lai!");
-		userController.registerEmployee(fullName, email, pass, projectId, proSkill);
-	}
+	
 }
